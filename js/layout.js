84e69554c4ccc1ef20b052e316b9662ee98af683
Mason <USER@L15>
2023-01-09 09:17:46 +0800

    ispanproject

--- js/layout.js
+++ js/layout.js
@@ -1,7 +1,7 @@
 $(document).ready(
     $("#back").delay(300).fadeOut(500),
     // $("#back").delay(1500).remove("#back"),
-$("#header").html(`<div class="container">
+    $("#header").html(`<div class="container">
 <h1><a class="navbar-brand" href="index.html"><img src="./img/LOGO.png" alt=""></a></h1>
 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
     aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,16 +29,25 @@ $("#header").html(`<div class="container">
         <li class="nav-item">
             <a class="nav-link" href="connection.html">聯絡我們</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item" >
             <a class="nav-link" href="javascript:void(0)" onclick="openMenberSide()"><i class="fa-solid fa-user"></i></a>
+        </li>
+        <li class="nav-item dropdown"style="display:none;">
+            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                <i class="fa-solid fa-cart-shopping"></i>
+            </a>
+            <div class="dropdown-menu cart-menu" id="cart-menu">
+            
+            </div>
         </li>
     </ul>
 </div>
 </div>`),
-$("#banner").html(`<div class="page-banner" style="height:300px;">
+    $("#banner").html(`<div class="page-banner" style="height:300px;">
 <div></div>
 </div>`),
-$("#footer").html(`<div class="container">
+    $("#footer").html(`<div class="container">
 <div class="row justify-content-between ">
     <div class="col-3 footer-info px-5 ">
         <h3 class="text-light">Information</h3>
@@ -69,6 +78,116 @@ $("#footer").html(`<div class="container">
     </div>
 </div>
 </div>`))
+$("#menber-layout").html(`<div id="menber-component">
+<div id="MenberSide" class="menberSide">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeMenberSide()">&times;</a>
+    <a href="javascript:void(0)" class="" data-bs-toggle="modal" data-bs-target="#LoginModal"
+        id="login">登入</a>
+    <p style="display:none;padding: 8px 8px 8px 32px;font-size: 25px;color: white;" id="welcome"></p>
+    <a href="javascript:void(0)" onclick="gomenber()">會員專區</a>
+    <a href="javascript:void(0)" onclick="logout()">登出</a>
+</div>
+<div class="modal fade" id="LoginModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content ">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">會員登入</h5>
+                <button type="button" class="btn-close" id="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" class="px-5" id="loginform"  method="">
+                    <div>
+                        <label for="account">會員帳號*</label><br>
+                        <input type="text"  id="account" style="width:100% ;"
+                            placeholder="請輸入帳號">
+                    </div>
+                    <div class="py-3">
+                        <label for="password">會員密碼*</label><br>
+                        <input type="password"  id="password" style="width:100% ;"
+                            placeholder="請輸入密碼">
+                    </div>
+                    <div class="text-center">
+                        <button type="button" class="loginSubmit" id="loginSubmit" 
+                            onclick="login()">登入</button>
+                    </div>
+                    <div class="text-center loginLink">
+                        <a href="javascript:void(0)" class="" data-bs-toggle="modal"
+                            data-bs-target="#registerModal"><i class="fa-solid fa-users"></i>立即註冊</a>
+                        <a href="javascript:void(0)" class="" data-bs-toggle="modal"
+                            data-bs-target="#forgetModal"><i class="fa-solid fa-question"></i>忘記密碼</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="registerModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">會員註冊</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" class="px-5" id="registerform">
+                    <div>
+                        <label for="registeraccount">帳號*</label><br>
+                        <input type="text" name="registeraccount" id="registeraccount" style="width:100% ;"
+                            placeholder="請輸入帳號" required>
+                    </div>
+                    <div class="py-2">
+                        <label for="registerpassword">密碼*</label><br>
+                        <input type="password" name="registerpassword" id="registerpassword"
+                            style="width:100% ;" placeholder="請輸入密碼" required>
+                    </div>
+                    <div class="">
+                        <label for="username">姓名*</label><br>
+                        <input type="text" name="registername" id="username" style="width:100% ;"
+                            placeholder="請輸入全名" required>
+                    </div>
+                    <div class="text-center registerbtn">
+                    <button type="button" class="forgetlogin" data-bs-toggle="modal"
+                    data-bs-target="#LoginModal">會員登入</button>
+                        <button type="button" class="registerSend" onclick="register()">確認送出</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="forgetModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">忘記密碼</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form action="" class="px-5">
+                    <div>
+                        <label for="forgetaccount">信箱*</label><br>
+                        <input type="text" name="forgetaccount" id="forgetaccount" style="width:100% ;"
+                            placeholder="請輸入電子郵件">
+                    </div>
+                    <div class="text-center forgetbtn">
+                        <button type="button" class="forgetlogin" data-bs-toggle="modal"
+                            data-bs-target="#LoginModal">會員登入</button>
+                        <button type="button" class="forgrtSend" onclick="forget()">送出</button>
+                    </div>
+                    <div class="text-center forgetLink">
+                        <a href="javascript:void(0)" class="" data-bs-toggle="modal"
+                            data-bs-target="#registerModal"><i class="fa-solid fa-user-plus"></i>立即註冊</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+</div>`)
 
 // 會員專區側邊攔
 function openMenberSide() {
--- js/login.js
+++ js/login.js
@@ -1,7 +1,27 @@
+
 var NowLogin = false;
+var menberList = [{
+    account: 'admin',
+    password: 'admin',
+    username: 'admin',
+    phonenum: '0912345678',
+    county: '台北市',
+    address: '信義路五段7號'
+}, {
+    account: 'user',
+    password: 'user',
+    username: 'User',
+    phonenum: '0987654321',
+    county: '台北市',
+    address: '信義路五段7號'
+},]
+var getmenberlist = "";
 $(document).ready(function () {
+    // var menstr = JSON.stringify(menberList)
+    // localStorage.setItem("menberList", menstr)
+    // getmenberlist = JSON.parse(localStorage.menberList)
     console.log(localStorage.UserName);
-    console.log(localStorage.NowLogin)
+    console.log(localStorage.NowLogin);
     if (JSON.parse(localStorage.NowLogin)) {
         $("#login").hide()
         $("#welcome").text("歡迎回來" + localStorage.UserName).show()
@@ -11,25 +31,10 @@ $(document).ready(function () {
     }
 })
 
-var menberList = [{
-    account: 'admin',
-    password: 'admin',
-    username: 'admin',
-    phonenum: '0912345678',
-    county: '台北市',
-    townships: '信義區',
-    address: '信義路五段7號'
-}, {
-    account: 'user',
-    password: 'user',
-    username: 'User',
-    phonenum: '0987654321',
-    county: '台北市',
-    townships: '信義區',
-    address: '信義路五段7號'
-},]
+
 var account = document.getElementById("account")
 var password = document.getElementById("password")
+var NowLogin = JSON.parse(localStorage.getItem('NowLogin'));
 account.addEventListener("keyup", function () {
     if (account.value == '' || account.value == null) {
         account.style['border'] = '1px red solid'
@@ -44,73 +49,256 @@ password.addEventListener("keyup", function () {
         password.style['border'] = '1px black solid'
     }
 })
+
 function login() {
-    if (account.value == '' || account.value == null) {
-        alert("請輸入帳號");
-        account.style['border'] = '1px red solid';
-        account.focus();
-        return false
-    } else if (password.value == '' || password.value == null) {
-        alert("請輸入密碼");
-        password.style['border'] = '1px red solid';
-        password.focus();
-        return false
-    } else {
-        // alert("登入成功");
-        for (var i = 0; i < menberList.length; i++) {
-            if (account.value == menberList[i].account) {
-                // alert("找到帳號" + menberList[i].account)
-                if (password.value == menberList[i].password) {
-                    alert("登入成功");
-                    NowLogin = true;
-                    localStorage.setItem("NowLogin", NowLogin)
-                    localStorage.setItem("UserName", menberList[i].username)
-                    return true;
-                } else {
-                    alert("密碼錯誤");
-                    password.style['border'] = '1px red solid';
-                    password.focus();
-                    return false
-                }
-            } else {
-                continue
+    var someaccont = menberList.some(function (item, index, array) {
+        return item.account == account.value;
+    })
+    var findaccont = menberList.find(function (item, index, array) {
+        return item.account == account.value;
+    })
+    if (getmenberlist == 0) {
+        if (account.value == '' || account.value == null) {
+            alert("請輸入帳號");
+            account.style['border'] = '1px red solid';
+            account.focus();
+        } else if (password.value == '' || password.value == null) {
+            alert("請輸入密碼");
+            password.style['border'] = '1px red solid';
+            password.focus();
+        } else if (someaccont) {
+            if (password.value == findaccont.password) {
+                alert("登入成功");
+                NowLogin = true;
+                localStorage.setItem("NowLogin", NowLogin)
+                localStorage.setItem("UserName", findaccont.username)
+                // document.location.href="/"
+                location.reload()
             }
+            else {  
+                alert("密碼錯誤");
+                password.style['border'] = '1px red solid';
+                password.focus();
+            }
+        } else { alert("查無此帳號")}
+    } else {
+        if (account.value == '' || account.value == null) {
+            alert("請輸入帳號");
+            account.style['border'] = '1px red solid';
+            account.focus();
+        } else if (password.value == '' || password.value == null) {
+            alert("請輸入密碼");
+            password.style['border'] = '1px red solid';
+            password.focus();
+        } else {
+            // alert("登入成功");
+            for (var i = 0; i < menberList.length; i++) {
+                if (account.value == getmenberlist[i].account) {
+                    // alert("找到帳號" + menberList[i].account)
+                    if (password.value == getmenberlist[i].password) {
+                        alert("登入成功");
+                        NowLogin = true;
+                        localStorage.setItem("NowLogin", NowLogin)
+                        localStorage.setItem("UserName", getmenberlist[i].username)
+                        // document.location.href="/"
+                        location.reload()
+                    } else {
+                        alert("密碼錯誤");
+                        password.style['border'] = '1px red solid';
+                        password.focus();
+                    }
+                } else {
+                    account.style['border'] = '1px red solid';
+                    continue
+                }
+            }
+            return false
         }
-        return false
     }
-    // document.location.href = "/";
-    NowLogin = true;
-    localStorage.setItem("NowLogin", NowLogin)
-};
+
+}
+// function login() {
+//     if (account.value == '' || account.value == null) {
+//         alert("請輸入帳號");
+//         account.style['border'] = '1px red solid';
+//         account.focus();
+//         return false
+//     } else if (password.value == '' || password.value == null) {
+//         alert("請輸入密碼");
+//         password.style['border'] = '1px red solid';
+//         password.focus();
+//         return false
+//     } else {
+//         // alert("登入成功");
+//         for (var i = 0; i < menberList.length; i++) {
+//             if (account.value == getmenberlist[i].account) {
+//                 // alert("找到帳號" + menberList[i].account)
+//                 if (password.value == getmenberlist[i].password) {
+//                     alert("登入成功");
+//                     NowLogin = true;
+//                     localStorage.setItem("NowLogin", NowLogin)
+//                     localStorage.setItem("UserName", getmenberlist[i].username)
+//                     // document.location.href="/"
+//                     location.reload()
+//                     return true;
+//                 } else {
+//                     alert("密碼錯誤");
+//                     password.style['border'] = '1px red solid';
+//                     password.focus();
+//                     return false
+//                 }
+//             } else {
+//                 account.style['border'] = '1px red solid';
+//                 continue
+//             }
+//         }
+//         return false
+//     }
+// };
+// function login() {
+//     if (account.value == '' || account.value == null) {
+//         alert("請輸入帳號");
+//         account.style['border'] = '1px red solid';
+//         account.focus();
+//         return false
+//     } else if (password.value == '' || password.value == null) {
+//         alert("請輸入密碼");
+//         password.style['border'] = '1px red solid';
+//         password.focus();
+//         return false
+//     } else {
+//         // alert("登入成功");
+//         for (var i = 0; i < menberList.length; i++) {
+//             if (account.value == menberList[i].account) {
+//                 // alert("找到帳號" + menberList[i].account)
+//                 if (password.value == menberList[i].password) {
+//                     alert("登入成功");
+//                     NowLogin = true;
+//                     localStorage.setItem("NowLogin", NowLogin)
+//                     localStorage.setItem("UserName", menberList[i].username)
+//                     // document.location.href="/"
+//                     location.reload()
+//                     return true;
+//                 } else {
+//                     alert("密碼錯誤");
+//                     password.style['border'] = '1px red solid';
+//                     password.focus();
+//                     return false
+//                 }
+//             } else {
+//                 account.style['border'] = '1px red solid';
+//                 continue
+//             }
+//         }
+//         return false
+//     }
+// };
+
+document.getElementById("btn-close").addEventListener("click", function () {
+
+    account.value = "";
+    password.value = "";
+})
 
 function logout() {
-    if (localStorage.getItem('NowLogin')) {
+    if (NowLogin) {
         NowLogin = false;
-        localStorage.setItem("NowLogin", NowLogin)
-        document.location.href = "/";
+        localStorage.setItem("NowLogin", NowLogin);
+        localStorage.removeItem("UserName");
+        // document.location.href = "/";
+        // location.reload()
+        history.back()
     }
-}
-function register(){
-    console.log($("#registerform"))
-    
 }
 
 // forgetaccount
 var forgetaccount = document.getElementById("forgetaccount")
+var emailRegxp = /^([\w]+)(.[\w]+)*@([\w]+)(.[\w]{2,3}){1,2}$/;
+// 監聽輸入時欄位的值是否為空，改變欄位樣式
 forgetaccount.addEventListener("keyup", function () {
-    if (forgetaccount.value == '' || forgetaccount.value == null) {
+    if (forgetaccount.value == '') {
         forgetaccount.style['border'] = '1px red solid'
     } else {
         forgetaccount.style['border'] = '1px black solid'
     }
 })
-function forget(){
-    if (forgetaccount.value == '' || forgetaccount.value == null) {
+function forget() {
+    if (forgetaccount.value == '') {
         alert("請輸入信箱");
         forgetaccount.style['border'] = '1px red solid';
         forgetaccount.focus();
         return false
-    }else{
+    } else if (emailRegxp.test(forgetaccount.value) != true) {
+        alert('電子信箱格式錯誤');
+        forgetaccount.style['border'] = '1px red solid';
+        forgetaccount.focus();
+        forgetaccount.value = "";
+        return false;
+    } else {
         alert("已送出")
+        forgetaccount.value = ""
+    }
+}
+function gomenber() {
+    if (JSON.parse(localStorage.NowLogin)) {
+        document.location.href = "menber.html"
+    } else {
+        alert("請先登入")
+    }
+}
+
+
+
+$("#registeraccount").on("keyup ", function () {
+    if ($("#registeraccount").val() == '') {
+        $("#registeraccount").css("border", "1px solid red");
+    } else {
+        $("#registeraccount").css("border", "1px solid black");
+    }
+})
+$("#registerpassword").on("keyup ", function () {
+    if ($("#registerpassword").val() == '') {
+        $("#registerpassword").css("border", "1px solid red");
+    } else {
+        $("#registerpassword").css("border", "1px solid black");
+    }
+})
+$("#username").on("keyup ", function () {
+    if ($("#username").val() == '') {
+        $("#username").css("border", "1px solid red");
+    } else {
+        $("#username").css("border", "1px solid black");
+    }
+})
+
+
+function register() {
+    if ($("#registeraccount").val() == '') {
+        alert("請輸入帳號");
+        $("#registeraccount").css("border", "1px solid red");
+        $("#registeraccount").focus()
+    } else if ($("#registerpassword").val() == '') {
+        alert('重新輸入密碼');
+        $("#registerpassword").css("border", "1px solid red");
+        $("#registerpassword").focus()
+    } else if ($("#username").val() == '') {
+        $("#username").css("border", "1px solid red");
+        $("#username").focus()
+    } else {
+        menberList.push({
+            account: $("#registeraccount").val(),
+            password: $("#registerpassword").val(),
+            username: $("#username").val(),
+            phonenum: '',
+            county: '',
+            address: ''
+        });
+        var menstr = JSON.stringify(menberList)
+        localStorage.setItem("menberList", menstr)
+        getmenberlist = JSON.parse(localStorage.menberList)
+        alert("註冊完成，請關閉視窗進行登入")
+        $("#registeraccount").val() = '';
+        $("#registerpassword").val() = '';
+        $("#username").val() = '';
     }
 }
